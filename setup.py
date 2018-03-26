a840faaa9fa3a6d54b577cea7c32b914983e39f9
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-03-26 18:04:51 +0200

    Bump to 0.2
    Fix tankersdk.crypto not being installed

--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ if sys.version_info.major < 3:
 
 
 setup(name="tankersdk-user-token",
-      version="0.1",
+      version="0.2",
       description="Tanker user token library",
       long_description="Building blocks to create your own user token server to use with the Tanker SDK",
       url="https://tanker.io",
       author="Kontrol SAS",
-      packages=["tankersdk.usertoken"],
+      packages=[
+          "tankersdk.crypto",
+          "tankersdk.usertoken",
+      ],
       install_requires=["PyNaCl"],
       classifiers=[
         "Programming Language :: Python :: 3.4",
