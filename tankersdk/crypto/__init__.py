095bbb6d3e16295f6538b4e7f8d8052cc15e08a6
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-03-30 14:56:21 +0200

    feat(crypto): only use the lower-level API of pynacl

--- tankersdk/crypto/__init__.py
+++ tankersdk/crypto/__init__.py
@@ -1,9 +1,8 @@
 from typing import Tuple
 
-import nacl.hash
-import nacl.signing
 from nacl.exceptions import BadSignatureError
-from nacl.encoding import RawEncoder
+from nacl.bindings.crypto_sign import crypto_sign, crypto_sign_keypair, crypto_sign_open, crypto_sign_BYTES
+from nacl.bindings.crypto_generichash import generichash_blake2b_salt_personal
 
 
 BLOCK_HASH_SIZE = 32
@@ -20,33 +19,22 @@ class InvalidSignature(Error):
 
 
 def generichash(buffer: bytes, *, size: int) -> bytes:
-    return nacl.hash.blake2b(buffer, digest_size=size, encoder=RawEncoder)
+    return generichash_blake2b_salt_personal(buffer, digest_size=size)
 
 
 def sign_keypair() -> Tuple[bytes, bytes]:
     """
     Generate a pair of signing keys and return a tuple (public_key, private_key)
     """
-    signing_key = nacl.signing.SigningKey.generate()
-    public_key = signing_key.verify_key
-    public_key_bytes = public_key.encode(encoder=RawEncoder)
-    # SigningKey.encode() would return the *seed*, not the value
-    private_key_bytes = signing_key._signing_key
-    return public_key_bytes, private_key_bytes
+    return crypto_sign_keypair()
 
 
 def sign_detached(message: bytes, private_key: bytes) -> bytes:
     """
     Sign a message with a private key and return the detached signature
     """
-    # We can't instanciate a signing key directly with the *value*
-    # so we instantiate it with a fake seed and then
-    # overwrite the ._signing_key member
-    fake_seed = b"\0" * 32
-    signing_key = nacl.signing.SigningKey(fake_seed)
-    signing_key._signing_key = private_key
-    signed_message = signing_key.sign(message, encoder=RawEncoder)
-    return signed_message.signature
+    raw_combined = crypto_sign(message, private_key)
+    return raw_combined[:crypto_sign_BYTES]
 
 
 def verify_sign_detached(message: bytes, signature: bytes, public_key: bytes) -> None:
@@ -54,7 +42,7 @@ def verify_sign_detached(message: bytes, signature: bytes, public_key: bytes) ->
     Verify the signature of the message given the signature and the public key
     """
     try:
-        public_key = nacl.signing.VerifyKey(public_key, encoder=RawEncoder)
-        public_key.verify(message, signature, encoder=RawEncoder)
+        combined = signature + message
+        crypto_sign_open(combined, public_key)
     except BadSignatureError:
         raise InvalidSignature()
