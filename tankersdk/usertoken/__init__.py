ac15b9b4ed17678f16bcac1c7bf26a0512c6ad9f
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-06-13 09:14:28 +0200

    refactor(usertoken): more meaningful variable names

--- tankersdk/usertoken/__init__.py
+++ tankersdk/usertoken/__init__.py
@@ -10,21 +10,21 @@ def generate_user_token(trustchain_id: str, trustchain_private_key: str, user_id
     trustchain_id_buf = base64.b64decode(trustchain_id)
     private_key_buf = base64.b64decode(trustchain_private_key)
 
-    user_id_buff = user_id.encode() + trustchain_id_buf
-
-    user_id = tankersdk.crypto.generichash(user_id_buff, size=BLOCK_HASH_SIZE)
+    user_id_buff = user_id.encode()
+    to_hash = user_id_buff + trustchain_id_buf
+    hashed_user_id = tankersdk.crypto.generichash(to_hash, size=BLOCK_HASH_SIZE)
 
     e_public_key, e_secret_key = tankersdk.crypto.sign_keypair()
-    to_sign = e_public_key + user_id
+    to_sign = e_public_key + hashed_user_id
     delegation_signature = tankersdk.crypto.sign_detached(to_sign, private_key_buf)
     random_buf = os.urandom(USER_SECRET_SIZE - 1)
-    hashed = tankersdk.crypto.generichash(random_buf + user_id, size=CHECK_HASH_BLOCK_SIZE)
+    hashed = tankersdk.crypto.generichash(random_buf + hashed_user_id, size=CHECK_HASH_BLOCK_SIZE)
     user_secret = random_buf + bytearray([hashed[0]])
 
     user_token = {
         "ephemeral_private_signature_key": base64.b64encode(e_secret_key).decode(),
         "ephemeral_public_signature_key": base64.b64encode(e_public_key).decode(),
-        "user_id": base64.b64encode(user_id).decode(),
+        "user_id": base64.b64encode(hashed_user_id).decode(),
         "delegation_signature": base64.b64encode(delegation_signature).decode(),
         "user_secret": base64.b64encode(user_secret).decode(),
     }
