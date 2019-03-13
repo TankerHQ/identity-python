4bc8ab99c317969d798bd3d393c98e2b6705859c
jerem <jeremy.tellaa@tanker.io>
2019-03-13 19:53:36 +0100

    adapt identity tests

--- tankersdk_identity/test/test_user_token.py
+++ tankersdk_identity/test/test_user_token.py
@@ -22,8 +22,8 @@ def test_generate_token_happy(test_trustchain):
     token = generate_test_token(test_trustchain)
     delegation_signature = base64.b64decode(token["delegation_signature"])
 
-    check_user_secret(token)
-    check_signature(test_trustchain["public_key"], token, delegation_signature)
+    check_user_secret(token, "user_id")
+    check_signature(test_trustchain["public_key"], token, delegation_signature, "user_id")
 
 
 def test_generate_token_invalid_signature(test_trustchain):
@@ -32,6 +32,4 @@ def test_generate_token_invalid_signature(test_trustchain):
     invalid_signature = corrupt_buffer(delegation_signature)
 
     with pytest.raises(tankersdk_identity.crypto.InvalidSignature):
-        check_signature(test_trustchain["public_key"], token, invalid_signature)
-
-
+        check_signature(test_trustchain["public_key"], token, invalid_signature, "user_id")
