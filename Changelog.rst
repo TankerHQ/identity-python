61fce585a7bb98a9ba61fc837053b841477f4d6c
jerem <jeremy.tellaa@tanker.io>
2019-03-14 15:50:06 +0100

    release 1.1.0

--- Changelog.rst
+++ Changelog.rst
@@ -1,3 +1,9 @@
+1.1
+===
+
+* Update to the new identity format
+* Bug fix in the user token upgrade
+
 1.0
 ===
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_long_description():
 
 setup(
     name="tankersdk-identity",
-    version="1.0.0",
+    version="1.1.0",
     description="Tanker identity library",
     long_description=get_long_description(),
     url="https://github.com/TankerHQ/identity-python",
